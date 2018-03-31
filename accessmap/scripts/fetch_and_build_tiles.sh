9380143bdb3697eadb376ee4a8bb479c853efa47
Nick Bolten <nbolten@gmail.com>
2018-03-31 16:58:16 -0700

    Initial elevator version

--- accessmap/scripts/fetch_and_build_tiles.sh
+++ accessmap/scripts/fetch_and_build_tiles.sh
@@ -1,10 +1,12 @@
 set -e
 
 datadir=$1
+branch=feature/snakemake
 
 # Fetch the data
-curl -L -o ${datadir}/sidewalks.geojson https://raw.githubusercontent.com/accessmap/accessmap-data/master/cities/seattle/data/sidewalk_network.geojson & \
-curl -L -o ${datadir}/crossings.geojson https://raw.githubusercontent.com/accessmap/accessmap-data/master/cities/seattle/data/crossing_network.geojson
+curl -L -o ${datadir}/sidewalks.geobuf https://github.com/AccessMap/accessmap-data/raw/$branch/cities/seattle/output/sidewalks_network.geobuf
+curl -L -o ${datadir}/crossings.geobuf https://github.com/AccessMap/accessmap-data/raw/$branch/cities/seattle/output/crossings.geobuf
+curl -L -o ${datadir}/elevator_paths.geobuf https://github.com/AccessMap/accessmap-data/raw/$branch/cities/seattle/output/elevator_paths.geobuf
 
 # Wait for parallelized processes to finish
 wait
@@ -12,4 +14,8 @@ wait
 mkdir -p ${datadir}/tiles
 
 # Build tiles
-tippecanoe -f -B 17 -z 17 -Z 12 -r 0 -L sidewalks:${datadir}/sidewalks.geojson -L crossings:${datadir}/crossings.geojson -e ${datadir}/tiles/pedestrian
+tippecanoe -f -B 17 -z 17 -Z 12 -r 0 \
+    -L sidewalks:${datadir}/sidewalks.geobuf \
+    -L crossings:${datadir}/crossings.geobuf \
+    -L elevator_paths:${datadir}/elevator_paths.geobuf \
+    -e ${datadir}/tiles/pedestrian
