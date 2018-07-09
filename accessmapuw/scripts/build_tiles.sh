1c99a63a9b328a2d546f13a16faa6a11185c6d43
= <=>
2018-07-09 11:32:31 -0700

    Added AccessMap UW deployment from uw branch

---
+++ accessmapuw/scripts/build_tiles.sh
@@ -0,0 +1,22 @@
+set -e
+
+datadir=$1
+branch=master
+
+mkdir -p ${datadir}/tiles
+
+# Build tiles
+tippecanoe -f -B 17 -z 17 -Z 10 -r 0 \
+    -L sidewalks:${datadir}/sidewalks.geojson \
+    -L crossings:${datadir}/crossings.geojson \
+    -L footways:${datadir}/footways.geojson \
+    -L stairs:${datadir}/stairs.geojson \
+    -e ${datadir}/tiles/paths
+
+tippecanoe -f -B 17 -z 17 -Z 10 -r 0 \
+    -L elevators:${datadir}/elevators.geojson \
+    -L kerbs:${datadir}/kerbs.geojson \
+    -e ${datadir}/tiles/points
+
+cp ${datadir}/paths.json ${datadir}/tiles/paths/tile.json
+cp ${datadir}/points.json ${datadir}/tiles/points/tile.json
