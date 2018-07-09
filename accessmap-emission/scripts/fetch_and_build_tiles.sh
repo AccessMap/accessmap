61d34bda4b93ef7e5d24df83322048d0b0d434bc
= <=>
2018-07-09 14:12:35 -0700

    Added emission study build dir

---
+++ accessmap-emission/scripts/fetch_and_build_tiles.sh
@@ -0,0 +1,21 @@
+set -e
+
+datadir=$1
+branch=master
+
+# Fetch the data
+curl -L -o ${datadir}/sidewalks.geobuf https://github.com/AccessMap/accessmap-data/raw/$branch/cities/seattle/output/sidewalks_network.geobuf
+curl -L -o ${datadir}/crossings.geobuf https://github.com/AccessMap/accessmap-data/raw/$branch/cities/seattle/output/crossings.geobuf
+curl -L -o ${datadir}/elevator_paths.geobuf https://github.com/AccessMap/accessmap-data/raw/$branch/cities/seattle/output/elevator_paths.geobuf
+
+# Wait for parallelized processes to finish
+wait
+
+mkdir -p ${datadir}/tiles
+
+# Build tiles
+tippecanoe -f -B 17 -z 17 -Z 10 -r 0 \
+    -L sidewalks:${datadir}/sidewalks.geobuf \
+    -L crossings:${datadir}/crossings.geobuf \
+    -L elevator_paths:${datadir}/elevator_paths.geobuf \
+    -e ${datadir}/tiles/pedestrian
