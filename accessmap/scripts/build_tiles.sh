4407ed98e4485981bf6b2c30ba7f43ff51b7acd6
Nick Bolten <nbolten@gmail.com>
2018-07-28 16:36:21 -0700

    Copied more centralized Caddyfile and host settings to main AccessMap website config

--- accessmap/scripts/build_tiles.sh
+++ accessmap/scripts/build_tiles.sh
@@ -1,8 +1,9 @@
 set -e
 
 datadir=$1
+host=$2
 
-mkdir -p ${datadir}/tiles
+mkdir -p ${datadir}/tiles/tilejson
 
 # Build tiles
 tippecanoe -f -B 17 -z 17 -Z 10 -r 0 \
@@ -11,4 +12,6 @@ tippecanoe -f -B 17 -z 17 -Z 10 -r 0 \
     -L elevator_paths:${datadir}/elevator_paths.geobuf \
     -e ${datadir}/tiles/pedestrian
 
-cp ${datadir}/pedestrian.json ${datadir}/tiles/pedestrian/tile.json
+cp ${datadir}/pedestrian.json ${datadir}/tiles/tilejson/pedestrian.json
+
+sed -i s,HOSTNAME,${host},g ${datadir}/tiles/tilejson/pedestrian.json
